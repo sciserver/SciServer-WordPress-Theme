38ffb56858e38d22e6afe4736a3251026ad90069
bonbons0220 <bsouter@jhu.edu>
2017-05-09 14:27:42 -0400

    updated theme for SciServer v1.10.0 Release

--- Gruntfile.js
+++ Gruntfile.js
@@ -31,7 +31,7 @@ module.exports = function(grunt) {
         'Gruntfile.js',
         'assets/js/*.js',
         '!assets/js/scripts.js',
-        '!assets/**/*.min.*'
+        '!assets/*/*.min.*'
       ]
     },
     less: {
