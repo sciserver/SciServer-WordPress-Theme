39ef0ebbd991c31c9273defeaa3ea729148d1d17
bonbons0220 <bsouter@jhu.edu>
2018-03-26 11:03:34 -0400

    started theme for mededsc

--- templates/content-sciserver-front-page.php
+++ templates/content-sciserver-front-page.php
@@ -19,83 +19,33 @@ if ( function_exists( 'get_cfc_meta' ) ) :
 endif;
 ?>
 </section>
-<?php $scis_options = get_option( "general-options" , array() );?>
 <section class="splash-banner" >
 <div class="container">
 <div class="row">
-<div class="col-sm-9">
-<h2><a href="/release/">SciServer <em><?php echo $scis_options[0]['release-name']; ?></em> <?php echo $scis_options[0]['system-version']; ?></a></h2>
-<?php while (have_posts()) : the_post(); ?>
-  <?php the_content(); ?>
-<?php endwhile; ?>
+<div class="col-sm-3">
+<a href="mailto:elbert@jhu.edu" class="btn btn-success" target="_blank" >About</a>
 </div>
 <div class="col-sm-3">
-<a href="//portal.sciserver.org" class="btn btn-success" target="_blank" >Login to SciServer</a>
+<a href="//hemi.jhu.edu/cmede/" class="btn btn-danger" target="_blank" >Visit CMEDE</a>
+</div>
+<div class="col-sm-3">
+<a href="//alpha.sciserver.org" class="btn btn-info" target="_blank" >Visit SciServer</a>
+</div>
+<div class="col-sm-3">
+<a href="//alpha02.sciserver.org" class="btn btn-warning" target="_blank" >Login to MEDEDSC</a>
 </div>
 </div>
 </div>
 </section>
-<section class="splash-teaser" >
-<?php 
-if ( function_exists( 'get_cfc_meta' ) ) : 
-$splash_teaser_ids = array();
-foreach( get_cfc_meta( 'splash-teaser' ) as $key => $value ){ 
-	$splash_teaser_ids[] = get_cfc_field( 'splash-teaser','page-number', false, $key );
-}
-$max_items = 6;
-$this_item = 1;
-?>
+<section class="splash-content" >
 <div class="container">
 <div class="row">
-<?php 
-	foreach ($splash_teaser_ids as $this_teaser_id) : 
-	
-		$this_teaser_post = get_post($this_teaser_id); 
-		
-		//title
-		$this_teaser_title = get_cfc_field('page-meta', 'display-name' , 	$this_teaser_id);
-		if ( empty( $this_teaser_title ) ) $this_teaser_title = $this_teaser_post->post_title; 
-		
-		//glyph
-		$this_teaser_glyph = get_cfc_field('page-meta', 'glyph' , 	$this_teaser_id);
-		if ( !empty( $this_teaser_glyph ) ) { $this_teaser_glyph = "<span class='glyphicon " . $this_teaser_glyph . "' aria-hidden='true'></span>"; }
-		
-		//blurb
-		$this_teaser_blurb = get_cfc_field('page-meta', 'blurb' , 	$this_teaser_id) ;
-		if ( empty( $this_teaser_blurb ) ) { $this_teaser_blurb = wp_trim_words( $this_teaser_post->post_content , 10 );}
-?>
-	<div class="col-md-2 col-sm-4 col-xs-6">
-		<div class="panel panel-splash-teaser panel-splash-teaser-<?php echo $this_item; ?>">
-			<div class="panel-heading"><a href="<?php echo get_page_link( $this_teaser_id ); ?>"><?php echo $this_teaser_glyph; ?></a>
-		  </div>
-			<div class="panel-body">
-		  <div class="entry-summary">
-			<h4 class="entry-title"><a href="<?php echo get_page_link( $this_teaser_id ); ?>"><?php echo $this_teaser_title; ?></a></h4>
-			<?php 
-			?>
-			<div class="hidden-sm hidden-xs"><?php 
-			if ( $this_teaser_id == get_option('page_for_posts') ) {
-				dynamic_sidebar('sidebar-primary');
-			} else {
-				echo $this_teaser_blurb;
-			}
-			?></div>
-		  </div>
-		  </div>
-		</div>
-	</div>
-<?php 
-		$this_item++;
-		if ($this_item>$max_items) {break;}
-	endforeach; 
-?>
+<div class="col-xs-12">
+<div class="content-box"><?php if ( have_posts(  ) ) { the_post(); the_content(); }?></div>
 </div>
-<?php
-endif; 
-?>
 </div>
 </div>
 </section>
 <?php
-//end Front Page Template.
+//End Splash Page Template.
 ?>
--- templates/pre-header.php
+++ templates/pre-header.php
@@ -26,13 +26,13 @@ if ( defined( 'WP_ENV' ) &&
 ?>
 	<div class="wrap container-fluid" role="document">
 		<div class="content row">
-			<div class="col-xs-12" style="background-color: yellow;">
+			<div class="col-xs-12 pre-header">
 				<p class="pull-right align-right">&nbsp;<br><?php
 				if (! is_user_logged_in()) {
 					echo( '<span><a href="/wp-login.php" class="btn btn-primary">Login to WordPress</a></span>' );
 				}?></p>
 			</div>
-			<div class="clearfix"><div>
+			<div class="clearfix"></div>
 		</div>
 	</div>
 <?php
