8de7c2ee55cb3fbffbf0103258cb6527a62b97e7
bonbons0220 <bsouter@jhu.edu>
2018-11-01 15:04:16 -0400

    updated gruntfile?

--- Gruntfile.js
+++ Gruntfile.js
@@ -18,6 +18,7 @@ module.exports = function(grunt) {
     'assets/vendor/bootstrap/js/scrollspy.js',
     'assets/vendor/bootstrap/js/tab.js',
     'assets/vendor/bootstrap/js/affix.js',
+    'assets/vendor/sciserver/js/*.js',
     'assets/js/plugins/*.js',
     'assets/js/_*.js'
   ];
