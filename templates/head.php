ded710be953c438de0810a43dca0015310cf3062
Jordan Raddick <raddick@jhu.edu>
2023-04-12 15:15:49 -0400

    Yay, grunt and less are working!

--- templates/head.php
+++ templates/head.php
@@ -4,9 +4,13 @@
   <meta charset="utf-8">
   <meta http-equiv="X-UA-Compatible" content="IE=edge">
   <meta name="viewport" content="width=device-width, initial-scale=1">
-<link href="<?php echo get_bloginfo('template_url') . '/assets/img/' . WEBSITE . '_favicon.ico'; ?>" rel="shortcut icon" type="image/x-icon" />
+  <link href="https://fonts.googleapis.com/css?family=Source+Sans+Pro:300,400,700,400italic" rel="stylesheet">
+  <link href="https://fonts.googleapis.com/css?family=Oswald:400,300,700&subset=latin,latin-ext" rel="stylesheet">
+  <link href="https://fonts.googleapis.com/css?family=family=Cabin:400,500,600,700,400italic,500italic,600italic,700italic" rel="stylesheet">
+  <link href="<?php echo get_bloginfo('template_url') . '/assets/img/' . WEBSITE . '_favicon.ico'; ?>" rel="shortcut icon" type="image/x-icon" />
 
   <link rel="alternate" type="application/rss+xml" title="<?php echo get_bloginfo('name'); ?> Feed" href="<?php echo esc_url(get_feed_link()); ?>">
 
   <?php wp_head(); ?>
 </head>
+
